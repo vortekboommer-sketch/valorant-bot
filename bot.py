9b3ffe9b9d913ac3d57f05778083295bdf5effd4
vortekboommer-sketch <vortek.boommer@gmail.com>
2026-02-26 23:00:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 # ─── CONFIG ───────────────────────────────────────────────────────────────────
-DISCORD_TOKEN = "MTQ3NjY4Njg1ODQ1NjEzNzcyOA.GIRnAs.pPR4-9MKlGFyQeQdc-hCLvl8tCvxmzscjuzfI0"
+DISCORD_TOKEN = "MTQ3NjY4Njg1ODQ1NjEzNzcyOA.GB7Tvi.YbM4XdAXvHZbSvkWeLtzjA8xXG2f_xGEtvO7VQ"
 CHANNEL_ID = 1310677185169850452
 RIOT_NAME = "zawn"
 RIOT_TAG = "7627"
